7cceab26fcdff7ddae0bdc23c954532fe3c9cce5
master-vibe <ashique.learnitallon@gmail.com>
2024-09-23 15:17:17 +0530

    Fixed a Syntax

--- expense-tracker.sh
+++ expense-tracker.sh
@@ -27,7 +27,7 @@ fi
 # Compile if needed (check for missing directory or specific class files)
 if [ "$res" = true ] || [ ! -f "$outputDir/Expense_Tracker.class" ]; then
     echo "Initializing..."
-    javac -cp "lib\jackson-core.jar;lib\jackson.jar" -d lib/classes src/CommandNotFoundException.java src/Expense_Tracker.java src/Expense.java src/Manager.java
+    javac -cp "lib/jackson-core.jar:lib/jackson.jar" -d lib/classes src/CommandNotFoundException.java src/Expense_Tracker.java src/Expense.java src/Manager.java
 
     if [ $? -ne 0 ]; then
         echo "Error: Compilation failed."
@@ -36,7 +36,7 @@ if [ "$res" = true ] || [ ! -f "$outputDir/Expense_Tracker.class" ]; then
 fi
 
 # Run the Java main class with additional arguments
-java -cp "lib\classes;lib\jackson-core.jar;lib\jackson.jar;lib\jackson-annotations.jar" Expense_Tracker $additionalArgs
+java -cp "lib/classes:lib/jackson-core.jar;lib/jackson.jar;lib/jackson-annotations.jar" Expense_Tracker $additionalArgs
 if [ $? -ne 0 ]; then
     echo "Error: Application execution failed."
     exit 1
